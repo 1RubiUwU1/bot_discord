0fde61ee7085a5013e73965ee1170ea00d757b58
Bi <110574193+1RubiUwU1@users.noreply.github.com>
2025-07-03 09:33:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def enviar():
 # ===================== EJECUCIÓN MULTIHILO ========================
 def run_flask():
     app.run(host="0.0.0.0", port=8080)
-
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
-    bot.run(TOKEN)
+
+bot.run(TOKEN)
